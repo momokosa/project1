141d1a4231844888ed0418c7bac39b28e8f2fa59
momokosa <momokosaa@gmail.com>
2018-11-21 17:23:59 +0700

    edit

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -4,10 +4,12 @@ class Login extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
 		$this->load->model('user_admin_model');
+		$this->load->helper('url');
 	}
 
 	public function index(){
-    	$this->load->view('login/index');
+		$data['title'] = 'Login';
+    	$this->load->view('login/index', $data);
 	}
 
 }
